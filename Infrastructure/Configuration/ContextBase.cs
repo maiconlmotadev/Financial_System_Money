90c3b10863af7f00a248cf568bc791d960254be4
maiconlmota <maiconlmotadev@hotmail.com>
2023-11-17 13:08:27 +0000

    Creating interface and generic repositories (finished)

--- Infrastructure/Configuration/ContextBase.cs
+++ Infrastructure/Configuration/ContextBase.cs
@@ -21,6 +21,27 @@ namespace Infrastructure.Configuration
         public DbSet<Category> Category { get; set; }
         public DbSet<Expense> Expense { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+        // Colocaremos a string de conexão aqui (Infrastruture) para ficar compilado (protegido)
+        // Podendo ficar também no projeto web (outra opção)
+
+        public string GetConnectionString()
+        {
+            return "Data Source=WINMACBOOK\\SQLEXPRESS;Initial Catalog=dbFinancialSystem; User ID=sa; Password=0000;";
+        }
     }
 }
